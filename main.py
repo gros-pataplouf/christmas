4bc2bb4efc5ce2730c62e5112199d08b3b72d038
gros-pataplouf <sceres@pm.me>
2024-11-29 12:16:25 +0100

    refactor: create switch function

--- main.py
+++ main.py
@@ -12,28 +12,27 @@ class Light():
 class Grid():
     def __init__(self, width : int, height : int):
         self.matrix = [[Light() for elt in range(0, width)] for elt in range(0, height)]
+
+    
+    def switch(self, coords: tuple, on=False, toggle=False):
+        (p1, p2) = coords
+        (x1, y1) = p1
+        (x2, y2) = p2
+        for i in range(y1, y2 + 1):
+            for j in range(x1, x2 + 1):
+                if not toggle:
+                    self.matrix[i][j].switch(on)
+                else:
+                    self.matrix[i][j].switch()
+
     
     def switch_on(self, coords : tuple):
-        (p1, p2) = coords
-        (x1, y1) = p1
-        (x2, y2) = p2
-        for i in range(y1, y2 + 1):
-            for j in range(x1, x2 + 1):
-                self.matrix[i][j].switch(True)
+        self.switch(coords, on=True)
+
 
     def switch_off(self, coords : tuple):
-        (p1, p2) = coords
-        (x1, y1) = p1
-        (x2, y2) = p2
-        for i in range(y1, y2 + 1):
-            for j in range(x1, x2 + 1):
-                self.matrix[i][j].switch(False)
+        self.switch(coords, on=False)
+
 
     def toggle(self, coords : tuple):
-        (p1, p2) = coords
-        (x1, y1) = p1
-        (x2, y2) = p2
-        for i in range(y1, y2 + 1):
-            for j in range(x1, x2 + 1):
-                self.matrix[i][j].switch()
- 
+        self.switch(coords, toggle=True)
