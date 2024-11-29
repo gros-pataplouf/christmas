e361e30d99c6c82cd0e2b1e39f3e2c30049d8eae
gros-pataplouf <sceres@pm.me>
2024-11-29 10:08:30 +0100

    test: lights can be switched on by coords

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ class Light():
     
     def switch(self, on=None):
         self.__is_on = on if on is not None else not self.__is_on
+
+class Grid():
+    def __init__(self, width : int, height : int):
+        self.matrix = [[Light() for elt in range(0, width)] for elt in range(0, height)]
+    
+    def switch_on(self, coords : tuple):
+        (p1, p2) = coords
+        (x1, y1) = p1
+        (x2, y2) = p2
+        for i in range(y1, y2 + 1):
+            for j in range(x1, x2 + 1):
+                self.matrix[i][j].switch(True)
--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import Light
+from main import Light, Grid
 
 def test_light_off_by_default() -> None:
     my_light : Light = Light()
@@ -20,3 +20,20 @@ def test_light_can_be_toggled() -> None:
     assert(my_light.is_on)
     my_light.switch()
     assert(not my_light.is_on)
+
+def test_1000x1000_grid_consists_of_lights() -> None:
+    my_grid : Grid = Grid(1000, 1000)
+    assert(len(my_grid.matrix) == 1000)
+    assert(len(my_grid.matrix[0]) == 1000)
+    assert(isinstance(my_grid.matrix[0][0], Light))
+
+def test_lights_in_grid_switched_on_by_coords() -> None:
+    my_grid : Grid = Grid(1000, 1000)
+    x1 : int = 0
+    y1 : int = 4
+    x2 : int = 5
+    y2 : int = 9
+    my_grid.switch_on(((x1, y1), (x2, y2)))
+    for i in range(y1, y2 + 1):
+        for j in range(x1, x2 + 1):
+            assert(my_grid.matrix[i][j].is_on)
